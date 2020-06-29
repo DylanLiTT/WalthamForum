0b18e769d1707a31b71da75538eb06de05ca4d5b
kevinjin813 <55816605+kevinjin813@users.noreply.github.com>
2020-06-28 20:16:39 -0400

    Add restaurant to the page

--- client/components/Restaurant.js
+++ client/components/Restaurant.js
@@ -3,11 +3,59 @@ import { Text, View, Button,StyleSheet,Image,ImageBackground} from 'react-native
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import background from '../assets/food.jpg';
-
+import Card from '../container/Card';
 
 export default function Restaurant({ route, navigation }) {
   return(
     <ImageBackground source={background} style={styles.backgroundImage}>
+         <Card>
+              <Text style={styles.highlight}> Mulan Taiwanese restaurant</Text>
+              <Text style={styles.text}> 835 Main Street</Text>
+              <Text style={styles.text}> 
+              {`
+                Mon. - Thu. & Sun.
+                11 am - 9:30 pm
+                Fri. & Sat
+                11 am - 10:30 pm
+                `
+              } 
+                
+              </Text>
+            </Card>    
+
+            <Card>
+              <Text style={styles.highlight}> Bonchon Waltham</Text>
+              <Text style={styles.text}> 329 Moody St</Text>
+              <Text style={styles.text}> 
+              {`
+                Mon. - Thu. & Sun.
+                11:30 am - 10 pm
+                Fri. & Sat
+                11:30 am - 10:30 pm
+                `
+              } 
+                
+              </Text>
+            </Card>    
+
+            <Card>
+              <Text style={styles.highlight}>Pho 1 Waltham</Text>
+              <Text style={styles.text}> 374 Moody St</Text>
+              <Text style={styles.text}> 
+              {`
+                Mon. - Thu. & Sun.
+                11:30 am - 10 pm
+                Fri. & Sat
+                11:30 am - 10:30 pm
+                Sun
+                12 pm - 10 pm
+                `
+              } 
+                
+              </Text>
+            </Card>   
+
+          
     </ImageBackground>
   );
 }
